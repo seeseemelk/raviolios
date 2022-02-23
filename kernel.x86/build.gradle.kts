2c90d58a2b164d1e3e3f4201b7c3e0127cb87162
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2022-02-23 21:45:56 +0100

    Working emulate and debug builds

--- kernel.x86/build.gradle.kts
+++ kernel.x86/build.gradle.kts
@@ -35,10 +35,48 @@ cpp {
     )
 }
 
+val isoDir = File(buildDir, "isoroot")
+val isoFile = File(buildDir, "iso/raviolios-x86.iso")
+
 tasks.register<Exec>("emulate") {
+    description = "Boots the ISO under Qemu"
     group = "application"
-    description = "Runs the kernel under Qemu"
-    //dependsOn(buildIso)
+
+    dependsOn("iso")
+    commandLine("qemu-system-i386", "-cdrom", isoFile, "-m", "16M", "-gdb", "tcp::1234", "-serial", "stdio")
+}
+
+tasks.register<Exec>("debug") {
+    description = "Boots the ISO under Qemu"
+    group = "application"
+
+    dependsOn("iso")
+    commandLine("qemu-system-i386", "-cdrom", isoFile, "-m", "16M", "-gdb", "tcp::1234", "-serial", "stdio", "-S")
+}
+
+tasks.register<Exec>("iso") {
+    description = "Generates the ISO file"
+    group = "build"
+    dependsOn("prepareIso")
+
+    doFirst {
+        isoFile.parentFile.mkdirs();
+    }
+
     workingDir(rootDir)
-    commandLine("qemu-system-i386", "-cdrom", "isoFile", "-m", "16M", "-gdb", "tcp::1234", "-serial", "stdio")
+    commandLine("grub-mkrescue", "-o", isoFile, isoDir)
+}
+
+tasks.register<Copy>("prepareIso") {
+    description = "Generates the ISO root directory"
+    dependsOn("icb", "assemble")
+
+    from(file("src/main/resources/isodir"))
+    from(tasks.getByName("icb").outputs) {
+        rename(".+\\.icb$", "boot/raviolios.icb")
+    }
+    from(File(buildDir, "cpp/output.elf")) {
+        rename(".+\\.elf$", "boot/raviolios-i386.elf")
+    }
+    into(isoDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,7 @@ rootProject.name = "raviolios"
 includeBuild("kernel.core")
 includeBuild("kernel.x86")
 includeBuild("kernel.test")
+includeBuild("lib.icb")
 includeBuild("plugin.raviolios-cpp")
+includeBuild("plugin.raviolios-icb")
 includeBuild("plugin.raviolios-java")
